6556475485d04be0cf2045c849c27c6a285c0f9d
roddms <kke3007@sookmyung.ac.kr>
2024-02-01 10:38:24 +0900

    0201

--- mypage/urls.py
+++ mypage/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path('my_bookmarks', views.my_bookmarks, name='my_bookmarks'),
     path('my_comments', views.my_comments, name='my_comments'),
     path('my_posts', views.my_posts, name='my_posts'),
-    # path('my_test', views.my_test, name='my_test'),
+    path('my_test', views.my_test, name='my_test'),
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -53,3 +53,30 @@ def my_comments(request):
         post.bookmarks_count = post.bookmarked.count()
     commented_posts = {comment.post for comment in my_comments}
     return render(request, 'my_comments.html', {'commented_posts': commented_posts})
+
+
+def my_test(request):
+
+    # 현재 로그인한 사용자를 context에 추가.
+    current_user = request.user
+    if current_user.is_authenticated:
+        # 로그인한 상태라면 현재 사용자의 정보를 context에 추가.
+        context = {
+            'current_user_name': current_user.username,
+            'current_user_nickname': current_user.nickname,
+            'current_user_email': current_user.email,
+            'current_user_gender': current_user.gender,
+            'current_user_birth': current_user.birth_date,
+            'current_user_bio': current_user.bio,
+            'current_user_user_type': current_user.user_type,
+            'current_user_profile_pic': current_user.profile_pic.url if current_user.profile_pic else None,
+        }
+    else:
+        # 로그인하지 않은 상태면 사용자 정보를 비워둠.
+        context = {
+            'current_user_nickname': None,
+            'current_user_profile_pic': None,
+            'current_user_user_type': None,
+        }
+
+    return render(request, 'my_test.html', context)
